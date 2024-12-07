77a0ed222c228a5d8d2226d0e6ee611e5e385369
JuliaKhvisevich <reytjulia@gmail.com>
2024-12-08 00:45:58 +0200

    feat: add answers

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -242,8 +242,15 @@ function convertNumberToString(numberStr) {
  *  '0123210'   => true
  *  'qweqwe'    => false
  */
-function isPalindrome(/* str */) {
-  throw new Error('Not implemented');
+function isPalindrome(str) {
+  let info = '';
+  for (let i = str.length - 1; i >= 0; i -= 1) {
+    info += str[i];
+  }
+  if (info === str) {
+    return true;
+  }
+  return false;
 }
 
 /**
@@ -260,8 +267,13 @@ function isPalindrome(/* str */) {
  *  'qwerty', 'Q'     => -1
  *  'qwerty', 'p'     => -1
  */
-function getIndexOf(/* str, letter */) {
-  throw new Error('Not implemented');
+function getIndexOf(str, letter) {
+  for (let i = 0; i < str.length; i += 1) {
+    if (str[i] === letter) {
+      return i;
+    }
+  }
+  return -1;
 }
 
 /**
@@ -279,8 +291,16 @@ function getIndexOf(/* str, letter */) {
  *  12345, 0    => false
  *  12345, 6    => false
  */
-function isContainNumber(/* num, digit */) {
-  throw new Error('Not implemented');
+function isContainNumber(num, digit) {
+  let currentNum = num;
+  while (currentNum > 0) {
+    const currentDigit = currentNum % 10;
+    if (currentDigit === digit) {
+      return true;
+    }
+    currentNum = Math.floor(currentNum / 10);
+  }
+  return false;
 }
 
 /**
